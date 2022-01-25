79ae21862bbf524e6fdf7487881aca275f7a0efc
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-01-25 21:02:42 +0200

    Add files via upload

---
+++ FirstProgram.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.11;
+
+contract FirstContract {
+    uint256 favouriteNumber = 5;
+    string favouriteString = "String";
+    int256 favouriteInt = -5;
+    bool favouriteBool = false;
+}
