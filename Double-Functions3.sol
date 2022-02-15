3e5e28cbad9717c0efd8f7bd611f568487e8ec7a
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-02-15 08:19:00 +0200

    Add files via upload

---
+++ Double-Functions3.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.7.0;
+
+contract G{
+    uint data = 10;
+
+    function x() public view returns(uint){
+        return data;
+    }
+
+    function y() public view returns(uint){
+        return data;
+    }
+}
