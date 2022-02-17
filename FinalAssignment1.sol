9adc506009a584f1516e07bbf6b9dbc5f3e4fa89
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-02-17 11:19:29 +0200

    Add files via upload

---
+++ FinalAssignment1.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.7.0;
+
+contract FirstAssignment{
+    uint a = 300;
+    uint b = 12;
+    uint f = 47;
+
+    function Finalized() public view returns(uint){
+        uint d = 23;
+        if(a >= a && b < f){
+            return (d*d)/b;
+        }
+    }
+}
