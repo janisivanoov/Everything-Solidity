c0c65672102b15cfa5a60f16eb29a51bf13d9368
Ivan(Janis) Ivanou <85873435+janisivanoov@users.noreply.github.com>
2022-03-05 11:54:58 +0200

    Add files via upload

---
+++ Inheritance.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+contract A {
+    uint innerVal = 100;
+
+    function innerAddTen(uint val) public pure returns(uint){
+        return val + 10;
+    }
+}
+
+contract B is A {
+    function outerAddTen(uint val) public pure returns(uint){
+        return A.innerAddTen(val);
+    }
+    function getInnerVal() public view returns(uint){
+        return A.innerAddTen();
+    }
+}
